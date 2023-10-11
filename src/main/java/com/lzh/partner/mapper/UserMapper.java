fc7d1487ca960eb8db02dd5e7f4ab3063a5d930f
xx <937815432@qq,com>
2023-10-11 23:16:11 +0800

    add: User实体类以及User的dao层

---
+++ src/main/java/com/lzh/partner/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.lzh.partner.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lzh.partner.model.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author 86132
+* @description 针对表【tag(标签)】的数据库操作Mapper
+* @createDate 2023-09-25 03:10:38
+* @Entity generator.domain.Tag
+*/
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
