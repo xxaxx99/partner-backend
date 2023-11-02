91b3b76d72ea27ea5209423ae553b247dd3bddc4
xx <937815432@qq,com>
2023-11-02 23:56:37 +0800

    add: 用户类以及用户队伍关系类VO类封装

---
+++ src/main/java/com/lzh/partner/model/vo/UserVO.java
@@ -0,0 +1,89 @@
+package com.lzh.partner.model.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Classname UserVO
+ * @Description 用戶队伍管理信息封装
+ * @Version 1.0.0
+ * @Date 2023/10/16 16:49
+ * @Created by lzh
+ */
+@Data
+public class UserVO implements Serializable {
+
+    private static final long serialVersionUID = 4570203714732525175L;
+    /**
+     * id
+     */
+    @TableId(type = IdType.AUTO)
+    private long id;
+
+    /**
+     * 用户昵称
+     */
+    private String username;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 性别
+     */
+    private String gender;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 个人简介
+     */
+    private String profile;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 标签列表 json
+     */
+    private String tags;
+
+    /**
+     * 状态 0 - 正常
+     */
+    private Integer userStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     *
+     */
+    private Date updateTime;
+
+
+    /**
+     * 用户角色 0 - 普通用户 1 - 管理员
+     */
+    private Integer userRole;
+
+
+}
