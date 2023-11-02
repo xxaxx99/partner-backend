86ccd4ed52e8854fa3d0af8c27c93519e96001d1
xx <937815432@qq,com>
2023-11-02 23:45:38 +0800

    add: 跨域全局配置

--- src/main/java/com/lzh/partner/config/WebMvcConfig.java
+++ src/main/java/com/lzh/partner/config/WebMvcConfig.java
@@ -6,9 +6,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 跨域配置
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -19,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 //设置允许跨域请求的域名
                 //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
-                .allowedOrigins("http://localhost:9527", "http://127.0.0.1:9527", "http://127.0.0.1:8082", "http://127.0.0.1:8083")
+                .allowedOriginPatterns("http://localhost:5173")
                 //是否允许证书 不再默认开启
                 .allowCredentials(true)
                 //设置允许的方法
